aa4a11502f9d4415f076a43f3f83cd72c9df1040
Bao Pham <pvhuybao2210@gmail.com>
2023-04-05 17:52:37 +0700

    complete base feature

--- api/index.js
+++ api/index.js
@@ -3,11 +3,28 @@ import postRoutes from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  console.log("file", file);
+  res.status(200).json(file.filename);
+  console.log(res.data);
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
